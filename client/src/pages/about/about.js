e61e2394bb1f900bcb4654bbda956db7165d7400
othman-dev <othmay@gmail.com>
2021-04-03 17:25:53 +0100

    content making

--- client/src/pages/about/about.js
+++ client/src/pages/about/about.js
@@ -3,16 +3,34 @@ import './about.scss';
 import Loading from '../../components/loading/loading.js';
 import Balise, { BaliseOn, BaliseOff } from '../../components/balise/balise.js';
 import { MainContext } from '../../context/mainContext.js';
+import { FaLinkedin, FaGithub, FaStackOverflow,  } from 'react-icons/fa';
+import { ReactComponent as Codingame} from './codingame.svg';
+import { dataEn, dataFr } from './aboutData.js';
 
 
 const About = () => {
 
-		const { dispatch } = useContext(MainContext);
+		const { main, dispatch } = useContext(MainContext);
 		
 		useEffect(() => {
 				dispatch({type: 'aboutPage'})
 		},[dispatch]);
 
+		function linkedinLink() {
+				window.open('https://www.linkedin.com/in/othman-abboud/', '_blank');
+		};
+
+		function githubLink() {
+				window.open('https://github.com/Othman-dev/Portfolio', '_blank');
+		};
+
+		function stackoverflowLink() {
+				window.open('https://stackoverflow.com/users/13862061/othman', '_blank');
+		};
+
+		function codingameLink() {
+				window.open('https://www.codingame.com/profile/d462ed371e6392c976083f23d1ad103d8533304', '_blank');
+		};
 
 return (
 		<div className='aboutMain'>
@@ -21,32 +39,38 @@ return (
 						<Balise/>
 						<div className='leftBlock'>
 								<BaliseOn content='h1'/>
-								<h1>{'test'}</h1>	
+								<h1>{!main.french ? dataEn.title : dataFr.title}</h1>	
 								<BaliseOff content='h1'/>
 						</div>
 						<div className='leftBlock'>
 								<BaliseOn content='p'/>
-								<p>{'test'}</p>
+								<p>{!main.french ? dataEn.content : dataFr.content}</p>
 								<BaliseOff content='p'/>
 						</div>
 				</div>
 				<div className='rightSide'>
 				<div className='aboutContainer'>
-						<div className='aboutLinksOdd'>
-								Linkedin
+						<span>{!main.french ? dataEn.linkTitle : dataFr.linkTitle}</span>
+						<div className='aboutLinksOdd' onClick={linkedinLink}>
+								<FaLinkedin className='AboutIconLink'/>
+								<p>{!main.french ? dataEn.linkedin : dataFr.linkedin}</p>
 						</div>
-						<div className='aboutLinksEven'>
-								Github
+						<div className='aboutLinksEven' onClick={githubLink}>
+								<p>{!main.french ? dataEn.stackoverflow : dataFr.stackoverflow}</p>
+								<FaStackOverflow className='AboutIconLink'/>
 						</div>
-						<div className='aboutLinksOdd'>
-								Stackoverflow
+						<div className='aboutLinksOdd' onClick={stackoverflowLink}>
+								<Codingame className='AboutIconLinkCodingame'/>
+								<p>{!main.french ? dataEn.codingame : dataFr.codingame}</p>
 						</div>
-						<div className='aboutLinksEven'>
-								CodinGame
+						<div className='aboutLinksEven' onClick={codingameLink}>
+								<p>{!main.french ? dataEn.github : dataFr.github}</p>
+								<FaGithub className='AboutIconLink'/>
 						</div>
 				</div>
 				</div>
 		</div>
 )};
+
 
 export default About;
---
+++ client/src/pages/about/aboutData.js
@@ -0,0 +1,19 @@
+export const dataEn = {
+		title: "Me, myself and I",
+		content: "I am a Fullstack developer located in Wales, UK. My goal is to create interactive, easy to use and efficient user experience.\n\nIndependant inquisitive mind and problem solver. I enjoy learning and testing myself to sharpen my skills, and produce more elaborate and efficient programs.\n\nAble to fill the Front-End and/or Back-end roles. Always ready to work with enthusiastic people on ambitious projects.",
+		linkTitle: "Use this link for more details about what I do",
+		linkedin: `My LinkedIn profile \n for more details`,
+		github: `Repo for this website \n and Github profile`,
+		stackoverflow: `My StackoverFlow profile \n where devs exchange`,
+		codingame: `My Codingame profile \n practicing algorithm`
+}
+
+export const dataFr = {
+		title: "Ce que je fais",
+		content: "test en francais",
+		linkTitle: "Utilisez ces liens pour plus de détails sur mon travail",
+		linkedin: `Mon profil Linkedin \n pour plus de détails`,
+		github: `Code de ce website \n et profil Github`,
+		stackoverflow: `Mon profil StackoverFlow \n entraide developpeur`,
+		codingame: `Mon profil Codingame \n pratiquer l'algorithmie`
+}
