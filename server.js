2ec707051de21cd355507ab5aea7718e8906d7e8
othman-dev <othmay@gmail.com>
2021-04-05 14:43:43 +0100

    deployement

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 app.use(express.json());
 
 app.use(cors());
 
 app.use((req, res, next) => {
-		res.setHeader('Access-Control-Allow-Origin', '*'); 
+		res.setHeader('Access-Control-Allow-Origin', 'https://res'); 
 		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
 		res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 		next();
 });
 
-app.get('/', (req, res) => res.send('API RUNNING'));
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+		app.use(express.static('client/build'));
+
+		app.get('*', (req, res) => {
+				res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		});
+}
 
 app.use('/api/email', require('./routes/email'))
 
